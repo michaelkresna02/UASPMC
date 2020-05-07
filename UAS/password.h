ab3ebb272cf6271763c28faec29307dd9d7b7570
michaelkresna02 <mikekresna@gmail.com>
2020-05-07 19:23:14 +0700

    Update password.h

--- UAS/password.h
+++ UAS/password.h
@@ -1,43 +1,34 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<conio.h>
 #include<string.h>
  
 void checkPass() {
-   char password[25], ch;
+   char password[55];
+   char username[55]; 
+   char ref2[25]="admin";
    char ref[25]="PMC";
-   int i;
-
-   system("cls");
    int cek=0;
 
    while(cek==0){
-       puts("Enter password : ");
- 
-   while (1) {
-      if (i < 0) {
-         i = 0;
-      }
-      ch = getch();
- 
-      if (ch == 13)
-         break;
-      if (ch == 8) /*ASCII value of BACKSPACE*/
-      {
-         putch('b');
-         putch(NULL);
-         putch('b');
-         i--;
-         continue;
-      }
- 
-      password[i++] = ch;
-      ch = '*';
-      putch(ch);
+    printf("Enter Username:\n");scanf("%s",username);printf("\n");
+    printf("Enter Password:\n"); 
+    int p=0; 
+    do{ 
+        password[p]=getch(); 
+        if(password[p]!='\r'){ 
+            printf("*"); 
+        } 
+        p++; 
+    }while(password[p-1]!='\r'); 
+    password[p-1]='\0';
+    if((strcmp(ref,password)==0)&&(strcmp(ref2,username)==0)){
+        cek=1;
+    } 
+    else if(cek==0){
+        printf("\n\nUsername atau Password salah, silahkan coba lagi\n\n");
+    }
    }
-   password[i] = '\0';
-   if(strcmp(ref,password)==0){
-       cek=1;
-   }
-   }
-   
+    printf("\nYou are in.\n");
+    printf("Press Any Key To Continue\n\n"); 
+    getch();
 }
