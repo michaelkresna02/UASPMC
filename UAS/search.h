ebb289e7d00fcce7430ab26504f28ef510602228
michaelkresna02 <mikekresna@gmail.com>
2020-05-07 14:39:28 +0700

    Update search.h

--- UAS/search.h
+++ UAS/search.h
@@ -13,56 +13,55 @@ void searching(){
     int found=0;
     int i=1;
     printf("No.\tNIK\t\tNama\t\tTmptLhr\tTglLhr\t\tUmur\tJns Klm\tGolDar\tStatus\tPekerjaan\n");
-    while(i<count){
         if(n==1){
-        while(strcmp(atoi(r),data[i].NIK)!=0){
+        while(i<count && strcmp(atoi(r),data[i].NIK)!=0){
         i++;
             }
         }
         else if(n==2){
-            while(strcmp(r,data[i].name)!=0){
+            while(i<count && strcmp(r,data[i].name)!=0){
             i++;
             found==1;
           }
         }
         else if(n==3){
-            while(strcmp(r,data[i].tempat)!=0){
+            while(i<count && strcmp(r,data[i].tempat)!=0){
             i++;
             found==1;
             }
         }
         else if(n==4){
-            while(strcmp(r,data[i].tanggal)!=0){
+            while(i<count && strcmp(r,data[i].tanggal)!=0){
             i++;
             found==1;
             }
         }
         else if(n==5){
-            while(strcmp(atoi(r),data[i].umur)!=0){
+            while(i<count && strcmp(atoi(r),data[i].umur)!=0){
             i++;
             found==1;
             }
         }
         else if(n==6){
-            while(strcmp(r,data[i].kelamin)!=0){
+            while(i<count && strcmp(r,data[i].kelamin)!=0){
             i++;
             found==1;
             }
         }
         else if(n==7){
-            while(strcmp(r,data[i].gol)!=0){
+            while(i<count && strcmp(r,data[i].gol)!=0){
             i++;
             found==1;
             }
         }
         else if(n==8){
-            while(strcmp(r,data[i].status)!=0){
+            while(i<count && strcmp(r,data[i].status)!=0){
             i++;
             found==1;
             }
         }
         else if(n==9){
-            while(strcmp(r,data[i].pekerjaan)!=0){
+            while(i<count && strcmp(r,data[i].pekerjaan)!=0){
             i++;
             found==1;
             }
@@ -71,8 +70,8 @@ void searching(){
             i=count;
         }
     displaySearch(i);
-    }
-    if(n==0){
+
+    if(found==0){
         printf("Data tidak ditemukan");
     }
 }
